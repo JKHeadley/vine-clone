e6f293e796933ac87fe1adabb154900a794bff82
Justin Headley <justin@scal.io>
2016-10-28 00:22:19 -0700

    Small fix/note.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,23 +5,23 @@
     .module('vineClone')
     .controller('MainController', MainController)
 
-  .directive('uiVideo', function () {
-    var vp; // video player object to overcome one of the angularjs issues in #1352 (https://github.com/angular/angular.js/issues/1352). when the videojs player is removed from the dom, the player object is not destroyed and can't be reused.
-    var videoId = Math.floor((Math.random() * 1000) + 100); // in random we trust. you can use a hash of the video uri
+    .directive('uiVideo', function () {
+      var vp; // video player object to overcome one of the angularjs issues in #1352 (https://github.com/angular/angular.js/issues/1352). when the videojs player is removed from the dom, the player object is not destroyed and can't be reused.
+      var videoId = Math.floor((Math.random() * 1000) + 100); // in random we trust. you can use a hash of the video uri
 
-    return {
-      scope: {
-        video: '='
-      },
-      restrict: 'E',
-      template: '<div class="video">' +
-      '<video ng-src="{{video.src}}" id="video-' + videoId + '" class="video-js vjs-default-skin" controls preload="auto" >' +
-      '</video></div>',
-      link: function (scope, element, attrs) {
-        vp = videojs('video-' + videoId, {width: 640, height: 480 });
-      }
-    };
-  });
+      return {
+        scope: {
+          video: '='
+        },
+        restrict: 'E',
+        template: '<div class="video">' +
+        '<video ng-src="{{video.src}}" id="video-' + videoId + '" class="video-js vjs-default-skin" controls preload="auto" >' +
+        '</video></div>',
+        link: function (scope, element, attrs) {
+          vp = videojs('video-' + videoId, {width: 640, height: 480 });//NOTE: videoId isn't actually changing between instances. Need to fix this.
+        }
+      };
+    });
 
   /** @ngInject */
   function MainController($log, $uibModal, $sce) {
